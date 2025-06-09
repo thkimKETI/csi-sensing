79ea89c7cd91b115a54daae9f154ace728287a2e
Taehyeon Kim <132858884+thkimKETI@users.noreply.github.com>
2025-06-09 13:42:24 +0900

    Create mqtt_config.py

---
+++ mqtt_config.py
@@ -0,0 +1,28 @@
+# mqtt_config.py
+
+import json
+from datetime import datetime
+
+# MQTT 설정
+BROKER_ADDRESS = "99.999.999.99" #INSERT YOUR BROKER ADDRESS
+PORT = 9999 # INSERT YOUR MQTT PORT 
+TOPIC = "/topic/KETI/csi" # INSERT YOUR MQTT TOPIC
+
+# 패킷 ID 관리
+packet_id = -1
+
+def get_next_packet_id():
+    global packet_id
+    packet_id += 1
+    if packet_id > 50000:
+        packet_id = 0
+    return packet_id
+
+def create_mqtt_message(loc=None, act=None,time=None):
+    message = {
+        "packet_id": get_next_packet_id(),
+        "timestamp": time,
+        "loc" : loc,
+        "act" : act,
+    }
+    return json.dumps(message)
